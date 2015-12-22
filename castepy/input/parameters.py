5c72ae94c35eb6a0903d1eaf62497f8ee8ce452b
Christian FERNANDEZ <christian.fernandez@ensicaen.fr>
2015-12-22 20:49:59 +0100

    modified notebooks - change iterator to list in parameters

--- castepy/input/parameters.py
+++ castepy/input/parameters.py
@@ -1,101 +1,104 @@
+from __future__ import print_function
+import sys
 import re
 import os.path
-
 from .valid_params import valid_params
 
 def makeparam(ss):
-  try:
-    ss = ss.strip().split()
-  except AttributeError:
-    return [ss]
-  else:
-    def try_cast(s):
-      try:
-        return int(s)
-      except:
-        try:
-          return float(s)
-        except:
-          return str(s)
+    try:
+        ss = ss.strip().split()
+    except AttributeError:
+        return [ss]
+    else:
+        def try_cast(s):
+            try:
+                return int(s)
+            except:
+                try:
+                    return float(s)
+                except:
+                    return str(s)
 
-    ss_conv = list(map(try_cast, ss))
+        ss_conv = list(map(try_cast, ss))
 
-    return ss_conv
+        return ss_conv
+
 
 class UnrecognisedParameter(Exception):
-  pass
+    pass
+
 
 class Parameters:
-  def __init__(self, params=None):
-    self.__dict__['params'] = {}
-    self.__dict__['comments'] = []
+    def __init__(self, params=None):
+        self.__dict__['params'] = {}
+        self.__dict__['comments'] = []
 
-    if params is not None:
-      if type(params) is dict:
-        self.params = params
-      elif type(params) is str:
-        if os.path.isfile(params):
-          self.params, self.comments = self.parse_params(open(params).read())
+        if params is not None:
+            if type(params) is dict:
+                self.params = params
+            elif type(params) is str:
+                if os.path.isfile(params):
+                    self.params, self.comments = self.parse_params(open(params).read())
+                else:
+                    self.params, self.comments = self.parse_params(params)
+            elif type(params) is file:
+                self.params, self.comments = self.parse_params(params.read())
+
+    def parse_params(self, params):
+        import re
+        split_re = re.compile("\s{0,}[=:]\s{0,}")
+        comments_re = re.compile("![^\n]+")
+
+        plines = params.split("\n")
+
+        comments = []
+        params = {}
+        for pline in plines:
+            pline = pline.strip()
+
+            if comments_re.match(pline):
+                comments.append(pline)
+            else:
+                pline = comments_re.sub("", pline)
+                nv = split_re.split(pline)
+
+                if len(nv) == 2:
+                    name = nv[0].strip()
+                    value = nv[1].strip()
+
+                    params[name] = makeparam(nv[1])
+
+        return params, comments
+
+    def __setitem__(self, n, v):
+        self.params[n] = v
+
+    def __setattr__(self, n, v):
+        if n in self.__dict__:
+            self.__dict__[n] = v
         else:
-          self.params, self.comments = self.parse_params(params)
-      elif type(params) is file:
-        self.params, self.comments = self.parse_params(params.read())
+            if n.lower() not in valid_params:
+                raise UnrecognisedParameter(n)
+            else:
+                if n in self.params and type(v) in [int, float]:
+                    self.params[n][0] = v
+                else:
+                    self.params[n] = makeparam(v)
 
-  def parse_params(self, params):
-    import re
-    split_re = re.compile("\s{0,}[=:]\s{0,}")
-    comments_re = re.compile("![^\n]+")
-
-    plines = params.split("\n")
-
-    comments = []
-    params = {}
-    for pline in plines:
-        pline = pline.strip()
-
-        if comments_re.match(pline):
-           comments.append(pline)
+    def __getitem__(self, n):
+        if n in self.params:
+            return self.params[n]
         else:
-          pline = comments_re.sub("", pline)
-          nv = split_re.split(pline)
-          
-          if len(nv) == 2:
-            name = nv[0].strip()
-            value = nv[1].strip()
+            raise KeyError
 
-            params[name] = makeparam(nv[1])
-    
-    return params, comments
-
-  def __setitem__(self, n, v):
-    self.params[n] = v
-  
-  def __setattr__(self, n, v):
-    if n in self.__dict__:
-      self.__dict__[n] = v 
-    else:
-      if n.lower() not in valid_params:
-        raise UnrecognisedParameter(n)
-      else:
-        if n in self.params and type(v) in [int, float]:
-          self.params[n][0] = v
+    def __getattr__(self, key):
+        if key in self.params:
+            return self.params[key]
         else:
-          self.params[n] = makeparam(v)
-    
-  def __getitem__(self, n):
-    if n in self.params:
-      return self.params[n]
-    else:
-      raise KeyError
+            raise AttributeError(key)
 
-  def __getattr__(self, key):
-    if key in self.params:
-      return self.params[key]
-    else:
-      raise AttributeError(key)
-
-  def __str__(self):
-    lines = []
-    lines += ["%s: %s" % (n , " ".join(map(str,v))) for n, v in list(self.params.items())]
-    lines += self.comments
-    return "\n".join(lines)
+    def __str__(self):
+        lines = []
+        lines += ["%s: %s" % (n, " ".join(map(str, v))) for n, v in self.params.items()]
+        lines += self.comments
+        return "\n".join(lines)
--- tests.py
+++ tests.py
@@ -1,9 +1,8 @@
 import unittest
-
 from unit_tests.test_cell import *
 from unit_tests.test_bonds import *
 from unit_tests.test_util import *
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
 
